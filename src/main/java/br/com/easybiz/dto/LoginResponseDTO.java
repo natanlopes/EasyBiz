3d98f963aa8e9096501b3e4004fe9655a855e5f6
Natanael lopes <natan1918@gmail.com>
2026-02-08 14:17:52 -0300

    refactor: converte LoginResponseDTO para class para forcar swagger

--- src/main/java/br/com/easybiz/dto/LoginResponseDTO.java
+++ src/main/java/br/com/easybiz/dto/LoginResponseDTO.java
@@ -3,9 +3,26 @@ package br.com.easybiz.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "Resposta de autenticação")
-public record LoginResponseDTO(
+public class LoginResponseDTO {
 
     @Schema(description = "Token JWT para autenticação do user", example = "eyJhbGciOiJIUzI1NiJ9...")
-    String token
+    private String token;
 
-) {}
+    // 1. Construtor Vazio (Obrigatório para o JSON/Jackson funcionar)
+    public LoginResponseDTO() {
+    }
+
+    // 2. Construtor Cheio (Para você usar no Controller: new LoginResponseDTO(token))
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    // 3. Getters e Setters (Obrigatórios para o Swagger ler o campo)
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
