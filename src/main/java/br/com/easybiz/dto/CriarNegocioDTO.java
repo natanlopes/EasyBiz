c84e2eecf32cb60a7d2fb552d51b684c5fb18db1
admArgofruta <146344230+admArgofruta@users.noreply.github.com>
2026-02-20 16:35:51 -0300

    sem limite de tamanho

--- src/main/java/br/com/easybiz/dto/CriarNegocioDTO.java
+++ src/main/java/br/com/easybiz/dto/CriarNegocioDTO.java
@@ -2,12 +2,13 @@ package br.com.easybiz.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Schema(description = "Dados necessarios para criacao de um negocio")
 public record CriarNegocioDTO(
         @Schema(example = "EasyBiz Barbearia", description = "Nome do negocio")
-        @NotBlank String nome,
+        @NotBlank @Size(max = 100) String nome,
 
         @Schema(example = "BARBEARIA", description = "Categoria do servico (Ex: Pedreiro, Manicure)")
-        @NotBlank String categoria
+        @NotBlank @Size(max = 50) String categoria
 ) {}
